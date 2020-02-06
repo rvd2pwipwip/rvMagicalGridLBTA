eaf5b756a13f23b308016ba182ffbe121575d1b4
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-06 17:05:27 -0500

    finished tutorial, cell zooming animations added

--- rvGridLBTA/ViewController.swift
+++ rvGridLBTA/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     private let borderWidth: CGFloat = 1
     private let numCellsPerRow: CGFloat = 15
+    private var selectedCell: UIView?
     
     // When the app creates the cells, it also puts them in a dictionary with their corresponding key (column/row)
     // The pan gesture will calculate which column/row it is touching so it can retrieve the corresponding cell from the dictionary
@@ -70,8 +71,28 @@ class ViewController: UIViewController {
         let numberOfRows = view.frame.height / cellSize
         let i = Int(location.x / cellSize) // calculate which column is being touched
         let j = Int(location.y / numberOfRows) // calaculate which row is being touched
-        let cellView = cells["\(i)|\(j)"] // get the corresponding cellView from the dictionary
-        cellView?.backgroundColor = .red
+        guard let cellView = cells["\(i)|\(j)"] else { return } // get the corresponding cellView from the dictionary
+        // unwrapped with optional binding (bring to front and animate without using ! or ?)
+        
+        
+        if selectedCell != cellView { // when selected cell is not the same as the touched cell, animate scale to original size
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.selectedCell?.layer.transform = CATransform3DIdentity
+            }, completion: nil)
+        }
+        
+        selectedCell = cellView // update which cell is selected
+        
+        view.bringSubviewToFront(cellView) // bring touched cell to front the animate scaling up
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            cellView.layer.transform = CATransform3DMakeScale(3, 3, 3)
+        }, completion: nil)
+        
+        if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0.25, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                cellView.layer.transform = CATransform3DIdentity
+            }, completion: nil)
+        }
     }
     
 }
