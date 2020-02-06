2d45f16e971efd8821a9580540a8439635f54927
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-06 07:18:21 -0500

    finished video 6, implemented hash map views dictionary

--- rvGridLBTA/ViewController.swift
+++ rvGridLBTA/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     private let borderWidth: CGFloat = 1
     private let numCellsPerRow: CGFloat = 15
     
+    // When the app creates the cells, it also puts them in a dictionary with their corresponding key (column/row)
+    // The pan gesture will calculate which column/row it is touching so it can retrieve the corresponding cell from the dictionary
+    private var cells = [String: UIView]() // instantiate cells dictionary with key of "column/row" and value of cellView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -35,6 +39,9 @@ class ViewController: UIViewController {
                     return cv
                 }()
                 view.addSubview(cellView)
+                
+                let key = "\(i)|\(j)" // set key to cell's row and column
+                cells[key] = cellView // set cellView at key in cells dictionnary
             }
         }
         view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
@@ -48,8 +55,23 @@ class ViewController: UIViewController {
     }
     
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
-        let location = gesture.location(in: view)
-        print(location)
+        let location = gesture.location(in: view) // get finger location as CGPoint coordinates
+        
+        // INEFFICIENT LOOP
+        // This loop goes through all the cells, one after the other until it finds the cell being touched (at location).
+        // Touching bottom row cells means the app goes through the loop a high number of times (depending on how many cells there are in the grid).
+//        for subview in view.subviews { // access all subviews of view
+//            if subview.frame.contains(location) { // check if cell is being touched
+//                subview.backgroundColor = .black
+//            }
+//        }
+        
+        let cellSize = view.frame.width / numCellsPerRow
+        let numberOfRows = view.frame.height / cellSize
+        let i = Int(location.x / cellSize) // calculate which column is being touched
+        let j = Int(location.y / numberOfRows) // calaculate which row is being touched
+        let cellView = cells["\(i)|\(j)"] // get the corresponding cellView from the dictionary
+        cellView?.backgroundColor = .red
     }
     
 }
